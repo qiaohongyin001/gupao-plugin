32d3a923cfd514e2f8a57eb591e11e1332c7df56
qiaohongyin001 <602573743@qq.com>
2019-03-26 00:41:53 +0800

    add .

---
+++ src/main/java/com/test/Test.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by HP on 2019/3/21.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello execute jar!");
+    }
+}
